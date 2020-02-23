175679dc42f7f0b4d75967c42c442a4675c4130d
John Hinz <jhinz402@hotmail.com>
2020-02-23 11:19:03 -0500

    Added file name to IPrediction

--- src/AIGaurd.Broker/IPrediction.cs
+++ src/AIGaurd.Broker/IPrediction.cs
@@ -5,7 +5,8 @@ namespace AIGaurd.Broker
     public interface IPrediction
     {
         public bool Success { get; set; }
-        public string base64Image { get; set; }
+        public string Base64Image { get; set; }
+        public string FileName { get; set; }
         public IDetectedObject[] Detections { get; set; }
 
     }
