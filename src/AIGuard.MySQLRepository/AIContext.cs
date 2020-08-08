01f410363a8a95f538d4142eaeda870d33602f32
John Hinz <jhinz402@hotmail.com>
2020-08-08 15:08:31 -0400

    Added MySQL Nuget and DBContext onModel* methods

--- src/AIGuard.MySQLRepository/AIContext.cs
+++ src/AIGuard.MySQLRepository/AIContext.cs
@@ -7,7 +7,27 @@ namespace AIGuard.MySQLRepository
 {
     class AIContext : DbContext
     {
+        private string _connectionStr;
+
+        public AIContext(string connectionStr)
+        {
+            _connectionStr = connectionStr;
+        }
         public DbSet<Capture> Captures { get; set; }
         public DbSet<Detection> Detections { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL(_connectionStr);
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Capture>(
+                e => e.HasMany(d => d.Detections));
+            
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
