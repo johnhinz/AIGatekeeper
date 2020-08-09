222b910483911f4c952a1528ae0f323b49e65406
John Hinz <jhinz402@hotmail.com>
2020-08-09 13:15:46 -0400

    Added column attributes to MySQL Detection.

--- src/AIGuard.MySQLRepository/Detection.cs
+++ src/AIGuard.MySQLRepository/Detection.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace AIGuard.MySQLRepository
@@ -7,11 +8,19 @@ namespace AIGuard.MySQLRepository
     public class Detection
     {
         public int Id { get; set; }
-        public string Target { get; set; }
+        [Column("label")]
+        public string Label { get; set; }
+        [Column("confidence")]
         public decimal Confidence { get; set; }
+        [Column("xmin")]
         public int XMin { get; set; }
+        [Column("xmax")]
         public int XMax { get; set; }
+        [Column("ymin")]
         public int YMin { get; set; }
+        [Column("ymax")]
         public int YMax { get; set; }
+        [Column("capture_id")]
+        public int CaptureId { get; set; }
     }
 }
