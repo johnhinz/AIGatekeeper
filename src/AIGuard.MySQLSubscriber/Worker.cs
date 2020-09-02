5ea2bbde1ac413e263c3129af0e2f3ed49b42892
John Hinz <jhinz402@hotmail.com>
2020-09-02 16:14:57 -0400

    Cleaned up log messages.

--- src/AIGuard.MySQLSubscriber/Worker.cs
+++ src/AIGuard.MySQLSubscriber/Worker.cs
@@ -45,46 +45,53 @@ namespace AIGuard.MySQLSubscriber
             };
             client.ApplicationMessageReceivedHandler = new MqttApplicationMessageReceivedHandlerDelegate(e =>
             {
-                var payload = JsonSerializer.Deserialize<Capture>(e.ApplicationMessage.Payload);
-                if (payload != null)
+                if (e.ApplicationMessage.Topic.Contains("False"))
                 {
-                    try
+                    _logger.LogInformation($"Message received on False queue");
+                }
+                else
+                {
+                    var payload = JsonSerializer.Deserialize<Capture>(e.ApplicationMessage.Payload);
+                    if (payload != null)
                     {
-                        _stopwatch.Start();
-                        _logger.LogInformation($"Message recieved {payload.FileName}");
-                        using (AIContext context = new AIContext(_mySQLConnectionString))
+                        try
                         {
-                            payload.dt = DateTime.Now;
-                            context.Captures.Add(payload);
-                            context.SaveChanges();
+                            _stopwatch.Start();
+                            _logger.LogInformation($"Message recieved {payload.FileName}");
+                            using (AIContext context = new AIContext(_mySQLConnectionString))
+                            {
+                                payload.dt = DateTime.Now;
+                                context.Captures.Add(payload);
+                                context.SaveChanges();
+                            }
+                            _stopwatch.Stop();
+                            _logger.LogInformation($"Message {payload.FileName} persited in {_stopwatch.ElapsedMilliseconds}ms");
                         }
-                        _stopwatch.Stop();
-                        _logger.LogInformation($"Message {payload.FileName} persited in {_stopwatch.ElapsedMilliseconds}ms");
-                    }
-                    finally
-                    {
-                        _stopwatch.Reset();
+                        finally
+                        {
+                            _stopwatch.Reset();
+                        }
                     }
                 }
             });
 
             client.ConnectedHandler = new MqttClientConnectedHandlerDelegate(async e =>
             {
-                _logger.LogInformation("### CONNECTED WITH SERVER ###");
+                _logger.LogInformation($"Connected to Mqtt");
                 await client.SubscribeAsync(new MqttTopicFilter { Topic = "AI/#", QualityOfServiceLevel = MqttQualityOfServiceLevel.AtMostOnce });
-                _logger.LogInformation("### SUBSCRIBED ###");
+                _logger.LogInformation($"Subscribed to Mqtt");
             });
 
             client.DisconnectedHandler = new MqttClientDisconnectedHandlerDelegate(async e =>
-            {
-                _logger.LogInformation("### DISCONNECTED FROM SERVER ###");
+            { 
+                _logger.LogInformation($"Disconnected from server");
                 try
                 {
                     await client.ConnectAsync(clientOptions);
                 }
-                catch
+                catch (Exception x)
                 {
-                    _logger.LogError("### RECONNECTING FAILED ###");
+                    _logger.LogError($"Reconnection failed {x.Message}");
                 }
             });
 
@@ -94,14 +101,13 @@ namespace AIGuard.MySQLSubscriber
             }
             catch (Exception exception)
             {
-                _logger.LogInformation("### CONNECTING FAILED ###" + Environment.NewLine + exception);
+                _logger.LogInformation($"Connection Failed {exception?.Message}");
             }
 
-            _logger.LogInformation("### WAITING FOR APPLICATION MESSAGES ###");
+            _logger.LogInformation("Waiting for messages.");
 
             while (!stoppingToken.IsCancellationRequested)
             {
-                //_logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 await Task.Delay(1000, stoppingToken);
             }
         }
