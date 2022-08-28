8f45c6681bf45266bd1d2c71da503c1c69dc6713
John Hinz <jhinz402@hotmail.com>
2022-08-28 15:03:04 -0400

    Initial code changes to use CodeProject.AI detector

---
+++ src/AIGuard.CodeProject.AI/DetectObjects.cs
@@ -0,0 +1,17 @@
+﻿using AIGuard.Broker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIGuard.CodePorject.AI
+{
+    internal class DetectObjects : IDetectObjects
+    {
+        public Task<IPrediction> DetectObjectsAsync(byte[] image, string imagePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
